37afab428d6400ca75466050081e51e38669dd93
Khush Vachhani <kapvm4444@gmail.com>
2025-07-12 16:34:03 +0530

    import error resolved

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -10,7 +10,7 @@ import {
 
 import styles from "./Map.module.css";
 import { useEffect, useState } from "react";
-import { useCities } from "../contexts/cityContext";
+import { useCities } from "../contexts/CityContext";
 import { useGeolocation } from "../hooks/useGeolocation";
 import { useUrlPosition } from "../hooks/useUrlPosition";
 import Button from "./Button";
